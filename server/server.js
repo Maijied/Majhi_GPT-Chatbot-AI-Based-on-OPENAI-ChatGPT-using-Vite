1b45298f57c78c5d06871bb06394b35d7ae3d8cf
Maizied Hasan <mdshuvo40@gmail.com>
2023-01-17 18:19:56 +0600

    changed max-token to 4000

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.post('/', async(req,res)=>{
             model:"text-davinci-003",
             prompt:`${prompt}`,
             temperature:0,
-            max_tokens:3000,
+            max_tokens:4000,
             top_p:1,
             frequency_penalty:0.5,
             presence_penalty:0,
