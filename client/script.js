4af9b118df559f6d01b543d9f293fbf522d647a4
Maizied Hasan <mdshuvo40@gmail.com>
2023-02-02 13:04:52 +0600

    Working on copy

--- client/script.js
+++ client/script.js
@@ -35,7 +35,7 @@ function typeText(element, text){
 function generateUniqueId(){
   const timestamp = Date.now();
   const randomNumber =Math.ceil(Math.random(0,99999));
-  const hexadecimalString = randomNumber.toString(16); 
+  const hexadecimalString = randomNumber.toString(2); 
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
