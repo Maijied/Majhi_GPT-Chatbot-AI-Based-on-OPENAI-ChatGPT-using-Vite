ceac01ca9b4b876f4f66af5ab55180a9359cf43a
maijied <mdshuvo40@gmail.com>
2023-02-13 12:54:12 +0600

    Working on feedback modal

--- client/script.js
+++ client/script.js
@@ -117,3 +117,48 @@ form.addEventListener('keyup', (e)=>{
 })
 
 
+
+// Modal 
+function dialog() {
+
+  // Declare variables
+  var dialogBox = $('.dialog'),
+      dialogTrigger = $('.dialog__trigger'),
+      dialogClose = $('.dialog__close'),
+      dialogTitle = $('.dialog__title'),
+      dialogContent = $('.dialog__content'),
+      dialogAction = $('.dialog__action');
+
+  // Open the dialog
+  dialogTrigger.on('click', function(e) {
+    dialogBox.toggleClass('dialog--active');
+    e.stopPropagation();
+  });
+
+  // Close the dialog - click close button
+  dialogClose.on('click', function() {
+    dialogBox.removeClass('dialog--active');
+  });
+
+  // Close the dialog - press escape key // key#27
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      dialogBox.removeClass('dialog--active');
+    }
+  });
+
+  // Close the dialog - click outside
+  $(document).on('click', function(e) {
+    if ($(e.target).is(dialogBox) === false &&
+        $(e.target).is(dialogTitle) === false &&
+        $(e.target).is(dialogContent) === false &&
+        $(e.target).is(dialogAction) === false) {
+      dialogBox.removeClass('dialog--active');   
+    }
+  });
+
+}
+
+// Run function
+$(dialog);
+// Modal End
